f7e883dc25eea33be951714188506374c9671eac
AnnKharko <73075905+AnnKharko@users.noreply.github.com>
2020-11-23 19:46:14 +0200

    JShw9 with async await

--- JShomeWorks/JShomeWork9/JShw9.js
+++ JShomeWorks/JShomeWork9/JShw9.js
@@ -335,7 +335,7 @@
 
 
 //================== після !!!!!!!!!!!!!   додано ще розвиток подій
-
+/*
 let chance = 0.2;
 function wakeUp(chance, cb) {
     console.log('Time to wake up');
@@ -547,3 +547,150 @@ wakeUp(chance,(err, data) => {
      })
     }
 }) 
+*/
+
+
+
+//========================== with async await ================
+
+ const chance = 0.3;
+function wakeUp(chance) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Time to wake up');
+            Math.random() > chance 
+            ? resolve('I have already woken up, and I am preparing breakfast for my daughter')
+            : reject('I oversleep')
+        }, 1000)
+
+    })
+}
+
+  function takeChildToSchool(chance){
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('We go to to school')
+            Math.random() > chance 
+            ? resolve('The child is already in school')
+            : reject('The child  is studying at home today')
+        }, 1000)
+      })
+  }
+
+
+  function cleanTheRoom(chance) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('I go to clean the room');
+            Math.random() > chance 
+            ? resolve('the house is clean')
+            : reject('I dont have time to clean the house')
+        }, 2000)
+      })
+  }
+
+  function toTheGroceryStore(chance) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('I go to the grocery store');
+            Math.random() > chance 
+            ? resolve('I bought everything I needed')
+            : reject(' no products')
+        }, 2000)
+      })
+   }
+
+
+   function cook(chance) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('I start cooking');
+            Math.random() > chance 
+            ? resolve('Eat already cooked')
+            : resolve('I have no products to cook')
+      },1000)
+   })  
+}
+   
+   function pickUpTheChildFromSchool() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+           console.log('It\'s time to pick up the child from school');
+           // Math.random() > chance 
+             resolve('Child at home');
+        }, 2000)
+      })
+  }
+
+  function doHomework(chance) {
+      return new Promise((resolve) => {
+       setTimeout(() => {
+        Math.random() > chance 
+        ? resolve('Great! I did it')
+        : resolve('The task is not yet completed')
+       }, 3000)
+      })
+  }
+
+  function putChildToSleep(chance) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            Math.random() > chance 
+            ? resolve('The child is slipping. I can finish hw')
+            : reject('Do not sleep and drink coffe!')
+        }, 1000)
+      })
+
+  }
+  function goToSleep(chance) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            Math.random() > chance 
+            ? resolve('Good night!')
+            : reject('Do not sleep and drink coffe!')
+        }, 1000)
+      })
+  }
+
+ async function day(chance) {
+     try { 
+     const wakeup = await wakeUp(chance);
+     console.log(wakeup);
+     const toSchool = await takeChildToSchool(chance);
+     console.log(toSchool);
+     const clean = await cleanTheRoom(chance);
+     console.log(clean);
+     const food = await cook(chance);
+     if (food === 'I have no products to cook') {
+         console.log(food);
+         const shop = await toTheGroceryStore(chance)
+         console.log(shop);
+     } else {console.log(food)};
+     
+     const picUpChild = await pickUpTheChildFromSchool();
+     console.log(picUpChild);
+     const hw = await doHomework(chance);
+     console.log(hw);
+    if (hw === 'The task is not yet completed') {
+        const childToSleep = await putChildToSleep(chance);
+        console.log(childToSleep);
+        const againHw = await doHomework(chance);
+        console.log(againHw);
+        if (againHw === 'The task is not yet completed') {
+            const result = 'Do not sleep and study!'
+            console.log(result);
+        } else {
+         const sleap = await goToSleep(chance);
+         console.log(sleap);
+        }
+    } else { 
+    const sleap = await goToSleep(chance);
+    console.log(sleap);
+    }
+    } catch(err) {
+     console.log(err);
+    }
+ }
+
+ day(chance);
+
